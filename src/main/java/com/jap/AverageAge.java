656c6d72fade1337308d7b338661380b30225ad8
rishavemail786 <rishavemail786@gmail.com>
2022-08-23 22:09:34 +0530

    SOLUTION DONE

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -2,21 +2,24 @@ package com.jap;
 
 public class AverageAge {
 
-    public String averageAgeCalculator(String[] ageRawData)
-    {
+    public String averageAgeCalculator(String[] ageRawData) {
+        String result = "";
+        double sum = 0;
         double avg = 0;
 
-        try{
+        try {
+            for (int i = 0; i < ageRawData.length; i++) {
+                sum += (Integer.parseInt(ageRawData[i]));
+                avg = sum / ageRawData.length;
+                result = String.valueOf(avg);
+            }
             //Write the logic
-
             //Handle specific exception
-        }catch (Exception exception)
-        {
+        } catch (Exception exception) {
             return exception.toString();
         }
-        return String.valueOf(avg);
+        return result;
     }
-
 
 
 }
